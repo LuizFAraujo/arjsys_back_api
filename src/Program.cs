8c8b03bc00a45c07b986c98f3d8474a38a3d39a9
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-11-28 21:39:20 -0300

    FEAT: Configuração inicial, com Swagger e SQLite
    - Adicionadas dependências necessárias para Swagger e SQLite
    - Configuração do Swagger no ambiente de desenvolvimento:
      - Swagger UI habilitado com endpoint '/swagger/v1/swagger.json'
    - Atualização do arquivo Program.cs para incluir as configurações:
      - Serviços de Controllers
      - Configuração do Swagger com AddSwaggerGen e UseSwaggerUI
      - Pipeline ajustado para HTTPS e mapeamento de Controllers

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+
+
+// Adiciona a configuração do Swagger (Documentação da API)
+builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger
+builder.Services.AddSwaggerGen(); // Gera o Swagger UI
+
+var app = builder.Build();
+
+// Habilita o Swagger apenas no ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    // Gera a documentação Swagger
+    app.UseSwagger();
+
+    // Configura o Swagger UI com o endpoint especificado
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArjSYS API v1"));
+}
+
+// Configura o pipeline de requisições HTTP
+app.UseHttpsRedirection();  // Redireciona para HTTPS
+app.UseAuthorization();  // Habilita a autorização (caso necessário)
+
+app.MapControllers();  // Mapeia os controllers da API
+
+app.Run();
